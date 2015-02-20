93555e719cb4af4dfddb2a2bf0d864275e1ab91b
Brandon Nguyen <phosphatide@gmail.com>
2015-02-19 17:38:45 -0800

    additional changes to inventory class

--- CIS-22B-Project/Inventory.h
+++ CIS-22B-Project/Inventory.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <string>
+
 class Inventory
 {
 private:
-	unsigned int ISBN;
-	string title, author, publisher;
-	unsigned int date, quantity;
-	double wholesale, retail;
+	unsigned int sISBN;
+	string sTitle, sAuthor, sPublisher;
+	unsigned int sDate, sQuantity;
+	double sWholesale, sRetail;
 
 public:
-	Inventory();
+	Inventory(unsigned int isbn, string title, string author, string publisher, unsigned int quantity, double wholesale);
+	void setPrice(double retail);
+	void addStock(unsigned int addition = 1);
+	void subStock(unsigned int subtraction = 1);
 	~Inventory();
 };
--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 #include "Inventory.h"
 
+using namespace std;
+
 int main()
 {
+	string test = "for color change if import works";
 	return 0;
 }
