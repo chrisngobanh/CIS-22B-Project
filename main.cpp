e60c43fd61273e0c197b8cc23462312ed64d427d
Andrew Morris <andrewmorrisftw@gmail.com>
2015-02-24 15:23:45 -0800

    started main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "Inventory.h"
-
+#include<iostream>
+#include"Inventory.h"
+#include<string>
 using namespace std;
 
 int main()
 {
-	string test = "for color change if import works";
+
+	int choice;
+	while (choice != 4)
+	{
+		cout << "Serendipity Booksellers\n";
+		cout << "Main Menu\n\n";
+		cout << "1. Cashier Module\n";
+		cout << "2. Inventory Database Module\n";
+		cout << "3. Report Module\n";
+		cout << "4. Exit\n\n";
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		while (choice < 1 || choice > 4)
+		{
+			cout << "Enter 1 - 4: ";
+			cin >> choice;
+		}
+
+		if (choice == 1)
+		//open cashier screen
+		else if (choice == 2)
+		//open inventory database screen
+		else if (choice == 3)
+		//open report screen
+		else if (choice == 4)
+		return 0;
+	}
+
 	return 0;
 }
